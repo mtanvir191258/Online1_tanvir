db14d6f1345cd509268527c22d8d1fc05ffcc6b2
First <memon191022@bscse.uiu.ac.bd>
2022-07-26 12:06:55 +0600

    7th commit

---
+++ cancel.php
@@ -0,0 +1,33 @@
+<?php
+
+	$name = $_GET["name"];
+
+	$date = $_GET["id"];
+	
+
+    
+
+
+
+	require_once('db_connect.php');
+
+	$connect = mysqli_connect( HOST, USER, PASS, DB )
+
+		or die("Can not connect");
+
+
+
+        mysqli_query( $connect, "DELETE FROM ticket_booking WHERE id=$id" )
+	
+
+		or die("Can not execute query");
+
+
+
+	echo "Record inserted:<br> name = $name <br> date = $date from=$from <br> to =$to" ;
+
+
+
+	echo "<p><a href=read.php>READ all records</a>";
+
+?>
---
+++ cancel_input.php
@@ -0,0 +1,18 @@
+<h1> CANCEL_TICKET_BOOKING</h1>
+
+
+
+<form method=get action=cancel.php>
+
+	Name: <input type=text name=name> <br>
+
+	<p>
+
+	To: <input type=text name=id> <br>
+
+
+	<p>
+
+	<input type=submit value=Insert>
+
+</form>
