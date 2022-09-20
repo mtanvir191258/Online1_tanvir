b0a5e30c9cd7a2034d78439a0c26f468dd3f1b04
mtanvir191258 <mtanvir191258@bscse.uiu.ac.bd>
2022-09-20 20:44:16 +0600

    appt

---
+++ aptt_input.php
@@ -0,0 +1,20 @@
+<h1>Create Record</h1>
+
+
+
+<form method=get action=aptt.php>
+
+	Name: <input type=text name=name> <br>
+
+	<p>
+
+	Date: <input type=date name=date> <br>
+	From: <input type=text name=from> <br>
+	To: <input type=text name=to> <br>
+
+
+	<p>
+
+	<input type=submit value=Insert>
+
+</form>
